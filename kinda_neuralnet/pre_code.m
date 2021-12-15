353b2700c0d1f784b9336cfdc3b48422e6e20c8f
Heramb Skanda <43760635+herambskanda@users.noreply.github.com>
2021-12-15 15:35:13 +0000

    Update pre_code.m

--- kinda_neuralnet/pre_code.m
+++ kinda_neuralnet/pre_code.m
@@ -3,12 +3,12 @@ data = randi(20,1,1000);
 dvt = randi(100,1,20);
 dvtd = dvt(data);
 
-a1 = fcon(@top_hat,data);
+a1 = fcon(@top_hat,data');
 
-a2 = onecon(@relu,a1,zeros(1,20),dvt);
+a2 = onecon(@relu,a1',zeros(1,20),dvt);
 
-unique(dvtd - a2')
-%a3 = onecon(@relu,a2,-(2:length(a2)+1),ones(1,length(a2)));
+check1 = unique(dvtd - a2') % should be zero
+
 a3 = a2+(1:length(a2))';
 
 a4 = fcon(@f,a3,(1:length(a3)),ones(1,length(a3)));
@@ -17,6 +17,7 @@ a4 = triu(a4,1);
 
 a5 = sum(a4,1);
 
+check2 = unique(domino(dvtd) - a5) % should be zero
 
 function out = f(in)
     out = in >= 0;
