02cdffea7865ac82916b9984a5861b1ed58bde19
Heramb Skanda <43760635+herambskanda@users.noreply.github.com>
2021-12-15 15:00:20 +0000

    Update domino.m

--- kinda_neuralnet/domino.m
+++ kinda_neuralnet/domino.m
@@ -1,21 +1,23 @@
-function output = domino(input_data,input_dvt)
+function [output,q] = domino(input_data,input_dvt)
     switch nargin 
         case 1 
             n = length(input_data);
             output = zeros(1,n);
+            input_data = input_data + (1:n);
             for i = 1:n
-                m = max(input_data(i),n);
+                m = min(input_data(i),n);
                 output(i+1:m) = output(i+1:m) + 1;
             end
         case 2
             dvt = zeros(1,max(unique(input_data)));
-            dvt(input_dvt) = 1;
-            input_dvt = dvt .* (1:max(unique(input_data)));
-            input_data = input_dvt(input_data);
+            dvt(unique(input_data)) = input_dvt;
+            input_dvt = dvt;
             n = length(input_data);
+            input_data = input_dvt(input_data) + (1:n);
+            q = input_data;
             output = zeros(1,n);
             for i = 1:n
-                m = max(input_data(i),n);
+                m = min(input_data(i),n);
                 output(i+1:m) = output(i+1:m) + 1;
             end
     end
